58d3386ea57bca7f38e5ef3d6952c11e89c80c78
nakamurakota <figalo66@gmail.com>
2021-10-19 11:37:07 +0900

    temp

--- src/index.ts
+++ src/index.ts
@@ -1,149 +1,157 @@
-import { loadEnv } from './dotenv';
-loadEnv();
+import { loadEnv } from './dotenv'
+loadEnv()
 
-import { App, MessageEvent } from '@slack/bolt';
-import { ConsoleLogger, LogLevel } from '@slack/logger';
-var lngDetector = new (require('languagedetect'));
+import { App, MessageEvent } from '@slack/bolt'
+import { ConsoleLogger, LogLevel } from '@slack/logger'
+var lngDetector = new (require('languagedetect'))()
 
-import * as middleware from './custom-middleware';
+import * as middleware from './custom-middleware'
 
-import { DeepLApi } from './deepl';
-import * as runner from './runnner';
-import * as reacjilator from './reacjilator';
+import { DeepLApi } from './deepl'
+import * as runner from './runnner'
+import * as reacjilator from './reacjilator'
 
-const logLevel = process.env.SLACK_LOG_LEVEL as LogLevel || LogLevel.INFO;
-const logger = new ConsoleLogger();
-logger.setLevel(logLevel);
+const logLevel = (process.env.SLACK_LOG_LEVEL as LogLevel) || LogLevel.INFO
+const logger = new ConsoleLogger()
+logger.setLevel(logLevel)
 
-const deepLAuthKey = process.env.DEEPL_AUTH_KEY;
+const deepLAuthKey = process.env.DEEPL_AUTH_KEY
 if (!deepLAuthKey) {
-  throw "DEEPL_AUTH_KEY is missing!";
+    throw 'DEEPL_AUTH_KEY is missing!'
 }
-const deepL = new DeepLApi(deepLAuthKey, logger);
+const deepL = new DeepLApi(deepLAuthKey, logger)
 
 const app = new App({
-  logger,
-  token: process.env.SLACK_BOT_TOKEN!!,
-  signingSecret: process.env.SLACK_SIGNING_SECRET!!
-});
-middleware.enableAll(app);
+    logger,
+    token: process.env.SLACK_BOT_TOKEN!!,
+    signingSecret: process.env.SLACK_SIGNING_SECRET!!
+})
+middleware.enableAll(app)
 
 // -----------------------------
 // shortcut
 // -----------------------------
 
-app.shortcut("deepl-translation", async ({ ack, body, client }) => {
-  await ack();
-  await runner.openModal(client, body.trigger_id);
-});
+app.shortcut('deepl-translation', async ({ ack, body, client }) => {
+    await ack()
+    await runner.openModal(client, body.trigger_id)
+})
 
-app.view("run-translation", async ({ ack, client, body }) => {
-  const text = body.view.state.values.text.a.value!;
-  const lang = body.view.state.values.lang.a.selected_option!.value;
+app.view('run-translation', async ({ ack, client, body }) => {
+    const text = body.view.state.values.text.a.value!
+    const lang = body.view.state.values.lang.a.selected_option!.value
 
-  await ack({
-    response_action: "update",
-    view: runner.buildLoadingView(lang, text)
-  });
+    await ack({
+        response_action: 'update',
+        view: runner.buildLoadingView(lang, text)
+    })
 
-  const translatedText: string | null = await deepL.translate(text, lang);
+    const translatedText: string | null = await deepL.translate(text, lang)
 
-  await client.views.update({
-    view_id: body.view.id,
-    view: runner.buildResultView(lang, text, translatedText || ":x: Failed to translate it for some reason")
-  });
-});
+    await client.views.update({
+        view_id: body.view.id,
+        view: runner.buildResultView(
+            lang,
+            text,
+            translatedText || ':x: Failed to translate it for some reason'
+        )
+    })
+})
 
-app.view("new-runner", async ({ body, ack }) => {
-  await ack({
-    response_action: "update",
-    view: runner.buildNewModal(body.view.private_metadata)
-  })
+app.view('new-runner', async ({ body, ack }) => {
+    await ack({
+        response_action: 'update',
+        view: runner.buildNewModal(body.view.private_metadata)
+    })
 })
 
 // -----------------------------
 // reacjilator
 // -----------------------------
 
-import { ReactionAddedEvent } from './types/reaction-added';
+import { ReactionAddedEvent } from './types/reaction-added'
 
-app.event("reaction_added", async ({ body, client }) => {
-
-  const event = body.event as ReactionAddedEvent;
-  if (event.item['type'] !== 'message') {
-    return;
-  }
-  const channelId = event.item['channel'];
-  const messageTs = event.item['ts'];
-  if (!channelId || !messageTs) {
-    return;
-  }
-  const lang = reacjilator.lang(event);
-  if (!lang) {
-    return;
-  }
-
-  const replies = await reacjilator.repliesInThread(client, channelId, messageTs);
-  if (replies.messages && replies.messages.length > 0) {
-    const message = replies.messages[0];
-    if (message.text) {
-      const translatedText = await deepL.translate(message.text, lang);
-      if (translatedText == null) {
-        return;
-      }
-      if (reacjilator.isAlreadyPosted(replies, translatedText)) {
-        return;
-      }
-      await reacjilator.sayInThread(client, channelId, translatedText, message);
+app.event('reaction_added', async ({ body, client }) => {
+    const event = body.event as ReactionAddedEvent
+    if (event.item['type'] !== 'message') {
+        return
     }
-  }
-});
+    const channelId = event.item['channel']
+    const messageTs = event.item['ts']
+    if (!channelId || !messageTs) {
+        return
+    }
+    const lang = reacjilator.lang(event)
+    if (!lang) {
+        return
+    }
+
+    const replies = await reacjilator.repliesInThread(
+        client,
+        channelId,
+        messageTs
+    )
+    if (replies.messages && replies.messages.length > 0) {
+        const message = replies.messages[0]
+        if (message.text) {
+            const translatedText = await deepL.translate(message.text, lang)
+            if (translatedText == null) {
+                return
+            }
+            if (reacjilator.isAlreadyPosted(replies, translatedText)) {
+                return
+            }
+            await reacjilator.sayInThread(
+                client,
+                channelId,
+                translatedText,
+                message
+            )
+        }
+    }
+})
 
 app.message('', async ({ body, client }) => {
     const event = body.event as MessageEvent
-    const channelId = event['channel'];
-    const messageTs = event['ts'];
-    const replies = await reacjilator.repliesInThread(client, channelId, messageTs);
-  if (replies.messages && replies.messages.length > 0) {
-    const message = replies.messages[0];
-    if (message.text) {
-        let formattedText = message.text.replace(/<(.*?)>/g, '')
-        let proposedLangs = lngDetector.detect(formattedText, 3).map( (val:any[][]) => val[0])
-        let lang = proposedLangs.length ? 'ja' : 'en'
+    const channelId = event['channel']
+    const messageTs = event['ts']
+    const replies = await reacjilator.repliesInThread(
+        client,
+        channelId,
+        messageTs
+    )
+    if (replies.messages && replies.messages.length > 0) {
+        const message = replies.messages[0]
+        if (message.text) {
+            const formattedText = message.text.replace(/<(.*?)>/g, '')
+            const proposedLangs = lngDetector
+                .detect(formattedText, 3)
+                .map((val: any[][]) => val[0])
+            const lang = proposedLangs.length ? 'ja' : 'en'
+            const translatedText = await deepL.translate(formattedText, lang)
 
-        console.log("proposedLangs")
-        console.log(proposedLangs)
-        // if(proposedLangs.length) {
-        //     let isEnglish = proposedLangs.some((val: string) =>{
-        //         ['english', 'hawaiian', 'italian', 'pidgin', 'danish'].includes(val)
-        //     })
-        //     console.log("isEnglish")
-        //     console.log(isEnglish)
-        //     lang = isEnglish ? 'ja' : 'en'
-        // }
-        console.log("lang")
-        console.log(lang)
+            if (translatedText == null) {
+                return
+            }
 
-      let translatedText = await deepL.translate(formattedText, lang);
-
-      if (translatedText == null) {
-        return
-      }
-
-      if (reacjilator.isAlreadyPosted(replies, translatedText)) {
-        return;
-      }
-      await reacjilator.sayInThread(client, channelId, translatedText, message);
+            if (reacjilator.isAlreadyPosted(replies, translatedText)) {
+                return
+            }
+            await reacjilator.sayInThread(
+                client,
+                channelId,
+                translatedText,
+                message
+            )
+        }
     }
-  }
-});
+})
 
 // -----------------------------
 // starting the app
 // -----------------------------
 
-(async () => {
-  await app.start(Number(process.env.PORT) || 3000);
-  console.log('⚡️ Bolt app is running!');
-})();
-
+;(async () => {
+    await app.start(Number(process.env.PORT) || 3000)
+    console.log('⚡️ Bolt app is running!')
+})()
